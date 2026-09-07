09b9d4b744123c8fb9c4630ae1c43fbff77fb35c
Diego Passos <diegopassos603@gmail.com>
2026-09-07 20:18:44 -0300

    .

--- Variáveis/variaveis.c
+++ Variáveis/variaveis.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main (){
+int main () {
 
+    
     // declaração de variáveis
     int idade;
     float altura;
     char inicial;
+    char nome[50];
 
     // Entrada de dados
     printf ("Digite sua idade: ");
@@ -14,6 +16,9 @@ int main (){
     printf ("Digite sua altura:");
     scanf ("%f", &altura);
 
+    printf ("Digite seu nome: ");
+    scanf (" %[^\n]", &nome); //Lê uma string com espaços até o usuário apertar enter
+
     printf ("Digite a inicial do seu nome: ");
     scanf (" %c", &inicial);
 
@@ -21,7 +26,9 @@ int main (){
     printf ("\n--- Dados Informados ---\n");
     printf ("Idade: %d\n", idade);
     printf ("Altura: %.2f\n", altura);
+    printf ("Nome: %s\n", nome);
     printf ("Inicial: %c\n", inicial);
+
 
     return 0;
 }
